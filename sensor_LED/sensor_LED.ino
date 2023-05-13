3ffeba501e9c31fd76a4328d72ecf60485ea0ff0
yyeonkim <seyeon.dev@gmail.com>
2023-05-13 20:51:42 +0900

    test: IR 센서 추가

--- sensor_LED/sensor_LED.ino
+++ sensor_LED/sensor_LED.ino
@@ -1,24 +1,27 @@
-int inputPin = 4;   // define ultrasonic signal receiver pin ECHO to D4
-int outputPin = 5;  // define ultrasonic signal transmitter pin TRIG to D5
+#include<IRremote.h>
+
+int edhoInputPin = 4;   // define ultrasonic signal receiver pin ECHO to D4
+int trigOutputPin = 5;  // define ultrasonic signal transmitter pin TRIG to D5
 int redLEDPin = 13;
 int greenLEDPin = 2;
+int irInputPin = 7;
 
 void setup() {
-
   Serial.begin(9600);
-  pinMode(inputPin, INPUT);
-  pinMode(outputPin, OUTPUT);
+  pinMode(edhoInputPin, INPUT);
+  pinMode(trigOutputPin, OUTPUT);
+  pinMode(irInputPin, INPUT);
 }
 
 void loop() {
   // 센서의 펄스 시간을 읽어서 거리로 변환
-  digitalWrite(outputPin, LOW);
+  digitalWrite(trigOutputPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(outputPin, HIGH);  // Pulse for 10μ s to trigger ultrasonic detection
+  digitalWrite(trigOutputPin, HIGH);  // Pulse for 10μ s to trigger ultrasonic detection
   delayMicroseconds(10);
-  digitalWrite(outputPin, LOW);
+  digitalWrite(trigOutputPin, LOW);
 
-  int distance = pulseIn(inputPin, HIGH);
+  int distance = pulseIn(edhoInputPin, HIGH);
   distance = distance / 58;
 
   if (distance <= 10) {
@@ -30,7 +33,8 @@ void loop() {
     digitalWrite(redLEDPin, HIGH);
     digitalWrite(greenLEDPin, LOW);
   }
+  
+  Serial.println(digitalRead(irInputPin));
 
-  Serial.println(distance);
   delay(100);
 }
